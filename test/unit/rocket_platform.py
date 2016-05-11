73ff489288e166653d8e1b08847708900b43cd0a
Will Angenent <will.angenent@ntt.eu>
2016-05-11 20:30:53 +0100

    Refactored platform functions for easier reuse

--- test/unit/rocket_platform.py
+++ test/unit/rocket_platform.py
@@ -11,29 +11,14 @@ class TestRocketBase(object):
         self.dbconn = sqlite_dbconn
         self.conn = sqlite_conn
 
-    def check_launch(self, schema, tables, expected_data,
-                     relations=None, global_relations=None,
-                     expected_fetch_count=None,
-                     one_subject=True):
-        if one_subject:
-            extraction_model_data = [{'subject': [{'tables': tables}]}]
-            if relations is not None:
-                extraction_model_data[0]['subject'].append(
-                    {'relations': relations})
-        else:
-            extraction_model_data = []
-            for t in tables:
-                subject = [{'tables': [t]}]
-                if relations is not None:
-                    subject.append({'relations': relations})
-                extraction_model_data.append({'subject': subject})
-
+    def check_launch(self, schema, extraction_model_data,
+                     expected_data, global_relations=None,
+                     expected_fetch_count=None, one_subject=True):
         if global_relations is not None:
             extraction_model_data.append({'relations': global_relations})
 
         extraction_model = ExtractionModel.load(schema, extraction_model_data)
         rocket = Rocket(self.dbconn, extraction_model).launch()
-
         expected_data = sorted(expected_data, key=lambda t: t[0].name)
 
         if rocket.flat_results() != expected_data:
@@ -50,11 +35,22 @@ class TestRocketBase(object):
     def check_one_subject(self, schema, tables, expected_data,
                           relations=None, global_relations=None,
                           expected_fetch_count=None):
-        self.check_launch(schema, tables, expected_data, relations=relations,
-                          global_relations=global_relations, one_subject=True)
+        extraction_model_data = [{'subject': [{'tables': tables}]}]
+        if relations is not None:
+            extraction_model_data[0]['subject'].append(
+                {'relations': relations})
+
+        self.check_launch(schema, extraction_model_data, expected_data,
+                          global_relations=global_relations)
 
     def check_two_subjects(self, schema, tables, expected_data,
                            relations=None, global_relations=None,
                            expected_fetch_count=None):
-        self.check_launch(schema, tables, expected_data, relations=relations,
-                          global_relations=global_relations, one_subject=False)
+        extraction_model_data = []
+        for t in tables:
+            subject = [{'tables': [t]}]
+            if relations is not None:
+                subject.append({'relations': relations})
+            extraction_model_data.append({'subject': subject})
+        self.check_launch(schema, extraction_model_data, expected_data,
+                          global_relations=global_relations)
